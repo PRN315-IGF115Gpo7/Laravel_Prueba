b72c77621e4efd1bef5804ba83ae57f6ae83576d
Fernando-Recinos <fernando.alonso2105@gmail.com>
2020-10-24 16:50:42 -0600

    Middleware en el Controlador

--- app/Http/Controllers/PuestoController.php
+++ app/Http/Controllers/PuestoController.php
@@ -12,6 +12,20 @@ use Illuminate\Http\Request;
 
 class PuestoController extends Controller
 {
+    
+
+     /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('log')->only('index');
+        $this->middleware('subscribed')->except('store');
+    }
+
     /**
      * Display a listing of the resource.
      *

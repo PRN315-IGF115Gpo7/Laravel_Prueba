18a3e4807e3f2fbf6f32d3afc6cde44efa2a5cfe
SamaelBermudez <ba18048@ues.edu.sv>
2020-10-24 13:14:50 -0600

    GrupoController Implementacion con recursos

--- app/Http/Controllers/GrupoController.php
+++ app/Http/Controllers/GrupoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Grupo;
 
 class GrupoController extends Controller
 {
@@ -13,7 +14,7 @@ class GrupoController extends Controller
      */
     public function index()
     {
-        //
+        return Grupo::all();
     }
 
     /**
